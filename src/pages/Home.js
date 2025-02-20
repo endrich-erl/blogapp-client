e5d22412706d375f9c5cbe16951c5bb395fbb09c
cabaerlendrich <cabaerlendrich@gmail.com>
2025-02-20 11:43:59 +0800

    added changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,37 @@
-import Banner from '../components/Banner';
+import { useState, useEffect } from "react";
+import Banner from "../components/Banner";
 
 export default function Home() {
+  const [user, setUser] = useState(null);
 
-    const data = {
-        title: "Blog Website",
-        content: "Post your blogs here",
-        destination: "/blogs",
-        buttonLabel: "Browse Blogs"
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      fetch(`https://blogapp-server-1-uj1f.onrender.com/users/details`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setUser({
+            id: data._id,
+            isAdmin: data.isAdmin,
+          });
+        })
+        .catch(() => {
+          setUser(null); // In case of an error, treat user as not logged in
+        });
     }
+  }, []);
 
-    return (
-        <>
-            <Banner data={data}/>
-        </>
-    )
+  const data = {
+    title: "Blog Website",
+    content: "Post your blogs here",
+    destination: user ? "/blogs" : "/login",
+    buttonLabel: user ? "Browse Blogs" : "Login to Access Blogs",
+  };
+
+  return <Banner data={data} />;
 }
